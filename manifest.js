878be705eba5805d0219935158e751de1c5fdd02
Luca 💣 <luca.testing96@gmail.com>
2025-12-08 00:40:24 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -3,7 +3,7 @@ function getManifest() {
         id: "org.corsaro.brain.v31.3",
         version: "1.3.0",
         name: "Leviathan",
-        description: "Deep Sea Streaming Core | AI Powered | ITA Priority",
+        description: "Motore streaming avanzato con ricerca intelligente, priorità contenuti ITA e link sempre aggiornati",
         logo: "https://img.icons8.com/ios-filled/500/00f2ea/dragon.png",
         resources: ["catalog", "stream"],
         types: ["movie", "series"],
